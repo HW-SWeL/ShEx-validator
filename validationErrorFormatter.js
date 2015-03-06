64deb36806f26f8ba046680a53f79d09494c5013
Tom Robertson <tomasrobertson@ymail.com>
2015-03-06 11:53:36 +0000

    fix for different rule object types

--- validationErrorFormatter.js
+++ validationErrorFormatter.js
@@ -1,7 +1,12 @@
 var RDF = require('./includes/Erics_RDF.js');
 
 function formatError(fail) {
-    var rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.type);
+    var rule;
+    if(fail.rule.valueClass._ === "ValueSet")
+        rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.values[0].term);
+    else
+        rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.type);
+
     return {
         name: fail._,
         triple : rule
