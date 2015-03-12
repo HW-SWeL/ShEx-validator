4bd8714a8d31636835b94f638e194a235d62da3c
Tom Robertson <tomasrobertson@ymail.com>
2015-03-12 17:00:22 +0000

    comment out validation error formatter untill it works better

--- validationErrorFormatter.js
+++ validationErrorFormatter.js
@@ -2,14 +2,14 @@ var RDF = require('./includes/Erics_RDF.js');
 
 function formatError(fail) {
     var rule;
-    if(fail.rule.valueClass._ === "ValueSet")
-        rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.values[0].term);
-    else
-        rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.type);
+    //if(fail.rule.valueClass._ === "ValueSet")
+    //    rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.values[0].term);
+    //else
+    //    rule = RDF.Triple(fail.rule.label, fail.rule.nameClass.term, fail.rule.valueClass.type);
 
     return {
         name: fail._,
-        triple : rule
+        triple : fail
     };
 }
 
